aaf81366d7147e819f202368d70d5d4a82f4681c
Yosibalacho <yosibala@gmail.com>
2022-06-25 22:14:22 +0300

    add

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ class Vehicle {
     this.engineCapacity = engineCapacity;
     this.color = color;
   }
+
+  static biggestEngine(array){
+     array.forEach(item => {
+      for (key in item){
+        
+      }
+     });
+  }
+
+ 
   printDetails() {
     return `number of wheels ${this.wheelsNum},engine capacity${this.engineCapacity},color${this.color}`;
   }
@@ -11,14 +21,22 @@ class Vehicle {
 // const details = new Vehicle(4, 1800, "black");
 // console.log(details);
 class Privte extends Vehicle {
-    constructor(wheelsNum,engineCapacity,color,name){
-          super(wheelsNum,engineCapacity,color)
-          this.name=name;
-    }
-          printDetails(){
-            return `${super.printDetails()},${this.name} 
-          }
-    }
+  constructor(wheelsNum, engineCapacity, color, name) {
+    super(wheelsNum, engineCapacity, color);
+    this.name = name;
+  }
+  uppercaseFirstAndLastLetter(){
+    return this.name[0].toUpperCase() + this.name.substring(1,this.name.length-1) + this.name[this.name.length-1].toUpperCase();
+  }
+  get result(){
+    return this.uppercaseFirstAndLastLetter()
+  }
+  printDetails() {
+    return `${super.printDetails()},${this.name}`;
+  }
 }
-// class Truck extends Vehicle{}
-// class Sport extends Vehicle{}
+const myPrivte=new Privte(21,260,"black","honda")
+console.log(myPrivte);
+
+class Truck extends Vehicle {}
+class Sport extends Vehicle {}
